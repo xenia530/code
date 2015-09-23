6eff22b7732f09b090d3962df1c0414a3376d5b8
xenia <1003674769@qq.com>
2015-09-23 04:53:03 +0100

    -a

---
+++ Movies/js/lanrentuku.js
@@ -0,0 +1,17 @@
+﻿/*浮动窗口*/
+(function(){
+	var n=0;
+	var obj=document.getElementById("AdLayer");
+	if(!obj){return false;}
+	var x=0;
+	var fe=$("#AdLayer");
+	window.onscroll=function(){
+		obj.style.top=(document.body.scrollTop||document.documentElement.scrollTop)+n+'px';
+		x=(document.body.scrollTop||document.documentElement.scrollTop)+n;
+		if(x==0){fe.fadeOut().hide()}else{fe.fadeIn().show()};
+	};
+	window.onresize=function(){obj.style.top=(document.body.scrollTop||document.documentElement.scrollTop)+n+'px'};
+})();
+
+/*懒人图库 www.lanrentuku.com */
+
